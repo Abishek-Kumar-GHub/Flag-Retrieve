19c933ba44454e79bbf19529c4f0920172eb5e90
Abishek-Kumar-GHub <abishek.techy@gmail.com>
2026-03-13 11:09:52 +0530

    gunicorn

--- app.py
+++ app.py
@@ -373,6 +373,6 @@ def points_reset():
 
 # ═══════════════════════════════════════════════════════════════════════════════
 
-if __name__ == '__main__':
-    _init_xml()          # create ctf_data.xml on first run
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
